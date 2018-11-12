55e6bb8fd9f0df3355476ccb8d5dd29c90904723
DilshadGit <dilshad.abdulla@icloud.com>
2018-11-12 00:50:23 +0000

    User login, logout, stop duplicate user which already taken, stop access page when user not login, install flask_login bcrypt password rehash the password when user registed

--- flask_pro/flaskpro/__init__.py
+++ flask_pro/flaskpro/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -9,10 +11,12 @@ app.config[
     'SECRET_KEY'] = '8ba7aa2eea1ddbf6c024bea56ac6c804198b535a1fe5616cced1d0c61f21ddb2'
 ''' configurate the db and location '''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_db.db'
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 ''' create the database '''
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'user_login'
+login_manager.login_message_category = 'info'
 
 from flaskpro import routes
--- flask_pro/flaskpro/routes.py
+++ flask_pro/flaskpro/routes.py
@@ -3,12 +3,18 @@ from flask import (
     url_for,
     flash,
     redirect,
+    request,
 )
-from flaskpro import app
+from flaskpro import app, db, bcrypt
 from flaskpro.forms import UserRegistrationForm, UserLoginForm
 ''' The models must imported after database created not before !!!! '''
 from flaskpro.models import User, Post
-
+from flask_login import ( 
+    login_user,
+    current_user,
+    logout_user,
+    login_required,
+)
 
 ''' 
 route in flask use to map to different pages, and decorator in flask use to add 
@@ -60,11 +66,20 @@ def about():
 
 @app.route('/account/register', methods=['GET', 'POST'])
 def user_register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     template_name = 'registration/registration_form.html'
     form = UserRegistrationForm()
     if form.validate_on_submit():
-        flash(f'You have been successfully registered for the username {form.username.data}',
-              'success')
+        ''' We ducrypte the password '''
+        hashed_pwd = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_pwd)
+        ''' Adding user to the database '''
+        db.session.add(user)
+        db.session.commit()
+        flash(f'You have been successfully registered. You are ready to login', 'success')
         return redirect(url_for('user_login'))
     context = {
         'title': 'Register',
@@ -74,12 +89,30 @@ def user_register():
 
 @app.route('/account/login', methods=['GET', 'POST'])
 def user_login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     template_name = 'registration/login_form.html'
     form = UserLoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'hello@gmail.com' and form.password.data == 'hello123':
-            flash('You have successfully logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Unsuccessful , please check the your detail', 'danger')
     return render_template(template_name, title='Login', form=form)
+
+
+@app.route('/account/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/user/account')
+@login_required
+def account():
+    template_name = 'registration/user_account.html'
+  
+    return render_template(template_name, title='account')
